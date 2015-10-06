08dd298ba9407d19de7341ae0bedf5ab07c253c6
Joe <joe@osdf.com>
2015-10-05 19:56:46 -0700

    Use exec, in project context, preserve stdout & stderr - JJW

--- webshell/views.py
+++ webshell/views.py
@@ -1,4 +1,4 @@
-import commands
+import sys, StringIO
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
@@ -9,6 +9,19 @@ from django.contrib.auth.decorators import permission_required
 @permission_required('is_superuser')
 def execute_script_view(request):
     source = request.POST.get('source', '').replace('"', r'\"')
-    result = commands.getoutput('python -c "%s"' % source)
+
+    sys.stdout = StringIO.StringIO()
+    sys.stderr = StringIO.StringIO()
+
+    try:
+        exec source
+    except Exception, e:
+        print e
+
+    result = 'stdout: \n%s\n\nstderr: \n%s' % (sys.stdout.getvalue(), sys.stderr.getvalue())
+
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
 
     return HttpResponse(result)
+    
